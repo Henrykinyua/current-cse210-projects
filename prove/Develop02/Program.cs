46d44700ddca4e710d1676a9fba00464fba208a2
Henrykinyua <kar21022@byui.edu>
2023-11-19 15:14:07 +0300

    complete journal assignment

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        while (true)
+        {  
+            Console.Write("Select one of the following");
+                     
+            Console.WriteLine("1. write a new entry");
+            Console.WriteLine("2. Display your journal");
+            Console.WriteLine("3. Load your journal from a  file");
+            Console.WriteLine("4. Save the journal to a file");
+            Console.WriteLine("5. Exit the program");
+            Console.Write("What would you like to do? ");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                Entry newEntry = new Entry();
+                newEntry._prompt = GetRandomPrompt();
+                Console.WriteLine('>'+newEntry._prompt);
+                newEntry._response = Console.ReadLine();
+                DateTime theCurrentTime = DateTime.Now;
+                newEntry._date = theCurrentTime.ToShortDateString();
+                journal._entries.Add(newEntry);
+            }
+            else if (choice == 2)
+            {
+                journal.Display();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("Enter the filename: ");
+                journal._filename = Console.ReadLine();
+                journal.LoadFile();
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Give a name to the file ");
+                journal._filename = Console.ReadLine();
+                journal.SaveFile();
+            }
+            else if (choice == 5)
+            {
+                break;
+            }
+             
+        }
+        static string GetRandomPrompt()
+        {
+            List<string> prompts = new List<string>()
+            {
+                "Who was the most interesting person I interacted with today?",
+                "What was the best part of my day?",
+                "How did I see the hand of the Lord in my life today?",
+                "What was the strongest emotion I felt today?",
+                "If I had one thing I could do over today, what would it be?"
+            };
+            Random random = new Random();
+            int index = random.Next(prompts.Count);
+            return prompts[index];
+        }
     }
 }
---
+++ prove/Develop02/entry.cs
@@ -0,0 +1,12 @@
+public class Entry 
+{
+    public string _date = "";
+    public string _prompt = "";
+    public string _response = "";
+
+    public void Format ()
+    {
+        Console.WriteLine($"Date: {_date} - prompt: {_prompt}\n{_response} ");
+        Console.WriteLine();
+    }
+}
